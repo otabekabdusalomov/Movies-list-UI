66ce165ce15c160ff45df4e8fe97040686f2344d
otabekabdusalomov <otabekabdusalamov2001@gmail.com>
2022-11-01 21:21:08 +0300

    movies project

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="app">
+    <div class="content">
+      <app-info />
+    </div>
+    <!-- <starter-information /> -->
+    
+  </div>
+</template>
+
+
+<script>
+import AppInfo from './Components/app-info/AppInfo.vue'
+import StarterInformation from './Course all/StarterInformation.vue'
+export default {
+  components: { 
+    StarterInformation,
+    AppInfo,
+  },
+  
+}
+</script>
+<style>
+.app {
+  height: 100vh;
+  color: black;
+}
+.content {
+  width: 1000px;
+  min-height: 700px;
+  background-color: #FFF;
+  margin: 0 auto;
+  padding: 5rem 0;
+  
+}
+</style>
---
+++ src/Course all/StarterInformation.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+      <h1>Counter: {{ counter }}</h1> 
+      <div class="d-flex">
+         <button @click="onIncrement" class="btn btn-success">Add</button>
+         <button v-on:click="onDecrement" class="btn btn-danger">Delite</button>
+      </div>   
+    </div>
+  </template>
+  
+  
+  <script>
+  export default {
+    data() {
+      return {
+        counter: 0,
+      }
+    },
+  
+    methods: {
+      onIncrement() {
+         this.counter += 1
+      },
+      onDecrement() {
+         this.counter -= 1
+      },
+    },
+  }
+  
+  </script>
+  
+  
+  <style>
+  
+  </style>
